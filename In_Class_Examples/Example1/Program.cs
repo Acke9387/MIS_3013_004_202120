e926e23576deb11defcba31d998214af849c61d5
Ackerman, Adam P <adamackrmn@gmail.com>
2022-02-07 23:29:43 -0600

    Added in class examples and Example1

---
+++ In_Class_Examples/Example1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Hello! Welcome to my application.  What is your name? >>");
+
+string name = Console.ReadLine();
+
+//Console.WriteLine(name + ", what is your total monthly income? >>");
+Console.WriteLine($"{name}, what is your total monthly income? >>");
+string totalMonthlyIncomeAsString = Console.ReadLine();
+double income = Convert.ToDouble(totalMonthlyIncomeAsString);
+
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+
+Console.WriteLine($"{name}, what is your total monthly expenses? >>");
+double expenses = Convert.ToDouble(Console.ReadLine());
+
+double amountSaved = income - expenses;
+
+Console.WriteLine($"{name}, you saved {amountSaved.ToString("C3")} this month!");
+//Console.Beep(5000, 500);
