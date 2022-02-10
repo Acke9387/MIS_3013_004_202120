1456efb019bfb8e18fc629b31ee71a43aadae2b8
Acke9387 <adam@ou.edu>
2022-02-10 11:24:46 -0600

    Added a loop for the coin toss

--- Participations/Conditional-Coin_Toss/Program.cs
+++ Participations/Conditional-Coin_Toss/Program.cs
@@ -3,47 +3,57 @@ const int TAILS = 2;
 const string DEVELOPER_NAME = "Professor Ackerman";
 
 Random random = new Random();
-int randomNumber = random.Next(1, 3);
 
 // For testing purposes so we know what to guess
 //Console.WriteLine(randomNumber);
+string answer;
+do
+{
+    int randomNumber = random.Next(1, 3);
+    Console.WriteLine("Heads or Tails? >>");
+    string guess = Console.ReadLine();
 
-Console.WriteLine("Heads or Tails? >>");
-string guess = Console.ReadLine();
+    int guessAsNumber = -1;
 
-int guessAsNumber = -1;
+    if (guess == "Heads")
+    {
+        guessAsNumber = HEADS;
+    }
+    else if (guess == "Tails")
+    {
+        guessAsNumber = TAILS;
+    }
+    else
+    {
+        //guessAsNumber = -1;
+        Console.WriteLine("Invalid entry.");
+        Environment.Exit(guessAsNumber);
+    }
 
-if (guess == "Heads")
-{
-    guessAsNumber = HEADS;
-}
-else if (guess == "Tails")
-{
-    guessAsNumber = TAILS;
-}
-else
-{
-    //guessAsNumber = -1;
-    Console.WriteLine("Invalid entry.");
-    Environment.Exit(guessAsNumber);
-}
+    if (randomNumber == HEADS)
+    {
+        Console.WriteLine("The coin came up Heads!");
+    }
+    else
+    {
+        Console.WriteLine("The coin came up Tails!");
+    }
 
-if (randomNumber == HEADS)
-{
-    Console.WriteLine("The coin came up Heads!");
-}
-else
-{
-    Console.WriteLine("The coin came up Tails!");
-}
+    if (randomNumber == guessAsNumber)
+    {
+        Console.WriteLine("Congratulations, you guessed correctly!");
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you came up short. =(");
+    }
 
-if (randomNumber == guessAsNumber)
-{
-    Console.WriteLine("Congratulations, you guessed correctly!");
-}
-else
-{
-    Console.WriteLine("Sorry, you came up short. =(");
-}
+
+    Console.WriteLine("Do you want to play again? yes or no >>");
+    answer = Console.ReadLine();
+
+    //} while (Console.ReadLine() == "yes");
+} while (answer == "yes");
+
 
 Console.WriteLine($"Developed by {DEVELOPER_NAME}.");
