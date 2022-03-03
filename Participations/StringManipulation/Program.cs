0e7f2bf1831389236761a99825be355a56b1be33
Acke9387 <adam@ou.edu>
2022-03-03 11:07:56 -0600

    Finished string manipulation participation

---
+++ Participations/StringManipulation/Program.cs
@@ -0,0 +1,28 @@
+﻿string sentence = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+
+Console.WriteLine(sentence);
+
+Console.WriteLine("What word would you like to replace? >>");
+string searchWord = Console.ReadLine();
+
+Console.WriteLine($"What would you like to replace {searchWord} with? >>");
+string replaceWord = Console.ReadLine();
+
+if (sentence.Contains(searchWord) == true)
+{
+    //Replace the searchword with replaceword
+    string newSentence = sentence.Replace(searchWord, replaceWord);
+    //sentence = sentence.Replace(searchWord, replaceWord);
+
+    Console.WriteLine(newSentence);
+}
+else
+{
+    Console.WriteLine($"Sorry, I could not find your word {searchWord}.");
+
+    for (int i = searchWord.Length - 1; i >= 0; i--)
+    {
+        Console.Write(searchWord[i]);
+    }
+
+}
