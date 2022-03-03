a9821fb8234a32112a1a9136984782e5123d0659
Acke9387 <adam@ou.edu>
2022-03-03 11:40:20 -0600

    Completed dictionary example

---
+++ In_Class_Examples/Collections-Dictionaries/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Create a dictionary that the key will be the student id and the value will be their GPA.
+ * Ask the user if there is another student they want to add, make sure it doesn’t exist then add it
+ * Ask them for a student id and output their GPA
+ * 
+ */
+
+Dictionary<int, double> students = new Dictionary<int, double>();
+students.Add(1, 2.5);
+students.Add(2, 4.0);
+students.Add(3, 3.7);
+
+Console.WriteLine("Do you want to add another student? yes or no >>");
+if (Console.ReadLine() == "yes")
+{
+    Console.WriteLine("What is their id? >>");
+    int tempId = Convert.ToInt32(Console.ReadLine());
+    if (students.ContainsKey(tempId) == false)
+    {
+        Console.WriteLine("What is their GPA? >>");
+        double tempGpa = Convert.ToDouble(Console.ReadLine());
+        students.Add(tempId, tempGpa);
+    }
+    else
+    {
+        Console.WriteLine($"{tempId} already exists. Not making any changes");
+    }
+}
+
+Console.WriteLine("Student ids: \t");
+foreach (int studentId in students.Keys)
+{
+    Console.Write(studentId + "\t");
+}
+
+Console.WriteLine();
+Console.WriteLine($"Which ID do you want to see the GPA of? >>");
+int id = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{id} has a gpa of {students[id]}");
---
+++ In_Class_Examples/Collections-Lists/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Using a List, ask the user to enter all of their favorite things.  
+ * Once they are done, randomly pick a value from the list and display it. * 
+ * 
+ */
+
+List<string> favoriteThings = new List<string>();
+
+do
+{
+    Console.WriteLine("Please enter one of your favorite things >>");
+    favoriteThings.Add(Console.ReadLine());
+
+    Console.WriteLine("Do you have another favorite thing to enter? yes or no >>");
+
+} while (Console.ReadLine().ToLower()[0] == 'y');
+
+Random rand = new Random();
+int randomIndex = rand.Next(0, favoriteThings.Count);
+Console.WriteLine("Random Favorite thing:");
+Console.WriteLine(favoriteThings[randomIndex]);
+
+//Output all favorite things.
+Console.WriteLine("All Favorite things:");
+foreach (string favoriteThing in favoriteThings)
+{
+    Console.WriteLine(favoriteThing);
+}
+
+//Equivalent for loop
+//for (int i = 0; i < favoriteThings.Count; i++)
+//{
+//    string favoriteThing = favoriteThings[i];
+//}
