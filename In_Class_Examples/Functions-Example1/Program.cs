98a24565b19c2af922603fa2ef7540a7960be0a4
Acke9387 <adam@ou.edu>
2022-03-22 11:10:34 -0500

    Completed first example over functions

---
+++ In_Class_Examples/Functions-Example1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine("Please enter your grade for Accounting as a double (e.g. .98, .75) <<");
+double accountingGrade = Convert.ToDouble(Console.ReadLine());
+
+string accountingLetter = ConvertGradeToLetterGrade(accountingGrade);
+
+Console.WriteLine($"{accountingGrade.ToString("P")} = {accountingLetter}");
+
+Console.WriteLine("Please enter your grade for Marketing as a double (e.g. .98, .75) <<");
+double marketingGrade = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"{marketingGrade.ToString("P")} = {ConvertGradeToLetterGrade(marketingGrade)}");
+
+
+static string ConvertGradeToLetterGrade(double grade)
+{
+    string letterGrade = "";
+
+    if (grade >= .90)
+    {
+        letterGrade = "A";
+    }
+    else if (grade >= .80)
+    {
+        letterGrade = "B";
+    }
+    else if (grade >= .70)
+    {
+        letterGrade = "C";
+    }
+    else if (grade >= .60)
+    {
+        letterGrade = "D";
+    }
+    else
+    {
+        letterGrade = "F";
+    }
+
+    return letterGrade;
+}
