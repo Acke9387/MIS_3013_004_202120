dd4c6262a7865ef2bb85f26df080c971e664ef23
Acke9387 <adam@ou.edu>
2022-03-29 10:43:36 -0500

    Completed the speak participation

---
+++ Participations/SimpleMethodSpeak/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Please enter an animal >>");
+string answer = Console.ReadLine();
+string animalSound = "";
+
+animalSound = Speak(answer);
+
+Console.WriteLine($"{answer} makes the sound {animalSound}");
+
+static string Speak(string animal)
+{
+    string result = "";
+
+    switch (animal)
+    {
+        case "Dog":
+            result = "Woof woof";
+            break;
+        case "Monkey":
+            result = "oooo oooo ahhh ahhh";
+            break;
+        case "Goat":
+            result = "people screaming";
+            break;
+        default:
+            result = "Grrrr";
+            break;
+    }
+
+    if (animal == "Dog")
+    {
+        result = "Woof woof";
+    }
+    else if (animal == "Monkey")
+    {
+        result = "oooo oooo ahhh ahhh";
+    }
+    else if (animal == "Goat")
+    {
+        result = "people screaming";
+    }
+    else
+    {
+        result = "Grrrr";
+    }
+
+        return result;
+}
