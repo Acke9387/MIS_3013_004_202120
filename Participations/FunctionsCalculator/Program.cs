0d5f90f8ecf50078a4af13beeff673db246f6395
Acke9387 <adam@ou.edu>
2022-04-12 11:32:29 -0500

    completed participations

---
+++ Participations/FunctionsCalculator/Program.cs
@@ -0,0 +1,111 @@
+﻿string courseName = "MIS-3013";
+
+DeveloperInformation("Instructor Ackerman", courseName, "4/12/2022");
+string goAgain;
+
+double answer = 0;
+bool shouldAskFirstQuestionoverFirstOperand = true;
+do
+{
+    Console.WriteLine($"Would you like to perform a Add, Subtract, Multiply or Divide calculation? >>");
+    string calculation = Console.ReadLine();
+    double lhOperand, rhOperand;
+
+    if (shouldAskFirstQuestionoverFirstOperand)
+    {
+        Console.WriteLine($"What is the first value? >>");
+        lhOperand = Convert.ToDouble(Console.ReadLine()); 
+    }
+    else
+    {
+        lhOperand = answer;
+    }
+
+    Console.WriteLine($"What is the second value? >>");
+    rhOperand = Convert.ToDouble(Console.ReadLine());
+    
+    switch (calculation.ToLower())
+    {
+        case "add":
+            answer = Add(lhOperand, rhOperand);
+            Console.WriteLine($"{lhOperand} + {rhOperand} = {answer}");
+            break;
+        case "subtract":
+            answer = Subtract(lhOperand, rhOperand);
+            Console.WriteLine($"{lhOperand} - {rhOperand} = {answer}");
+            break;
+        case "multiply":
+            answer = Multiply(lhOperand, rhOperand);
+            Console.WriteLine($"{lhOperand} * {rhOperand} = {answer}");
+            break;
+        case "divide":
+            answer = Divide(lhOperand, rhOperand);
+            Console.WriteLine($"{lhOperand} / {rhOperand} = {answer}");
+            break;
+        default:
+            break;
+    }
+
+    Console.WriteLine("Do you want to perform another calculation? yes or no >>");
+    goAgain = Console.ReadLine();
+
+    if (goAgain.ToLower() == "yes")
+    {
+        Console.WriteLine($"Do you want to use {answer} as your first value? yes or no>>");
+
+        if (Console.ReadLine().ToLower() == "yes")
+        {
+            shouldAskFirstQuestionoverFirstOperand = false;
+        }
+        else
+        {
+            shouldAskFirstQuestionoverFirstOperand = true;
+        }
+
+    }
+} while (goAgain.ToLower()[0] == 'y');
+
+
+
+
+/// <summary>
+/// Add's two values up and returns the sum
+/// </summary>
+static double Add(double num1, double num2)
+{
+    double result = 0;
+
+    result = num1 + num2;
+
+    return result;
+}
+
+/// <summary>
+/// Subtract's the two values and returns the difference
+/// </summary>
+static double Subtract(double num1, double num2)
+{
+    return num1 - num2;
+}
+
+static double Multiply(double num1, double num2)
+{
+    return num1 * num2;
+}
+
+static double Divide(double num1, double num2)
+{
+    if (num2 == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return num1 / num2;
+    }
+}
+
+static void DeveloperInformation(string name, string course, string date)
+{
+    Console.WriteLine($"{name} wrote this application for {course} on {date}");
+}
---
+++ Participations/MethodsWithParallelArrays/Program.cs
@@ -0,0 +1,52 @@
+﻿string[] names = { "John", "Sally", "Jane", "Will", "Herman" };
+int[] ids = { 1, 2, 3, 4, 5 };
+List<List<double>> grades = new List<List<double>>();
+//grades.Add(new List<double>());
+//grades[0].Add(.54);
+//grades[0].Add(.578);
+//grades[0].Add(.56);
+grades.Add(CreateRandomSizedListWithRandomValues());
+grades.Add(CreateRandomSizedListWithRandomValues());
+grades.Add(CreateRandomSizedListWithRandomValues());
+grades.Add(CreateRandomSizedListWithRandomValues());
+grades.Add(CreateRandomSizedListWithRandomValues());
+
+
+//StudentInformation(names[0], ids[0], grades[0]);
+//StudentInformation(names[1], ids[1], grades[1]);
+
+for (int i = 0; i < names.Length; i++)
+{
+    StudentInformation(names[i], ids[i], grades[i]);
+}
+
+static void StudentInformation(string name, int id, List<double> grades)
+{
+    double average = 0;
+    double sum = 0;
+    foreach (double grade in grades)
+    {
+        sum += grade;
+    }
+    //sum = grades.Sum();
+    //average = sum / grades.Count();
+    //average = grades.Average();
+    average = sum/ grades.Count;
+
+    Console.WriteLine($"{name} ({id}) has {grades.Count.ToString("N")} # of grades and has an average of {average.ToString("P")}!");
+}
+
+static List<double> CreateRandomSizedListWithRandomValues()
+{
+    List<double> randoGrades = new List<double>();
+    Random rand = new Random();
+    int numberOfGrades = rand.Next(3, 8);
+
+    for (int i = 0; i < numberOfGrades; i++)
+    {
+        double grade = rand.NextDouble() ;
+        randoGrades.Add(grade);
+    }
+
+    return randoGrades;
+}
