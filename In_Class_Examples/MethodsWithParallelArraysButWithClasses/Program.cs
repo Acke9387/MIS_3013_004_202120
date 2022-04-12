5f41415159dc35b537eeebe0654f5f26a8167d22
Acke9387 <adam@ou.edu>
2022-04-12 11:45:45 -0500

    Did in class example over classes

---
+++ In_Class_Examples/MethodsWithParallelArraysButWithClasses/Program.cs
@@ -0,0 +1,49 @@
+﻿string[] names = { "John", "Sally", "Jane", "Will", "Herman" };
+int[] ids = { 1, 2, 3, 4, 5 };
+
+List<Student> students = new List<Student>();
+//Student student1 = new Student();
+//student1.Name = "John";
+//student1.Id = 1;
+//student1.Grades = CreateRandomSizedListWithRandomValues();
+for (int i = 0; i < names.Length; i++)
+{
+    Student newStudent = new Student();
+    newStudent.Name = names[i];
+    newStudent.Id = ids[i];
+    newStudent.Grades = CreateRandomSizedListWithRandomValues();
+
+    students.Add(newStudent);
+}
+//Student student2 = new Student();
+//student2.Name = "Sally";
+//student2.Id = 2;
+//student2.Grades = CreateRandomSizedListWithRandomValues();
+
+
+//StudentInformation(names[0], ids[0], grades[0]);
+//StudentInformation(names[1], ids[1], grades[1]);
+
+foreach (Student student in students)
+{
+    //student.StudentInformation();
+
+    Console.WriteLine(student);
+}
+
+
+
+static List<double> CreateRandomSizedListWithRandomValues()
+{
+    List<double> randoGrades = new List<double>();
+    Random rand = new Random();
+    int numberOfGrades = rand.Next(3, 8);
+
+    for (int i = 0; i < numberOfGrades; i++)
+    {
+        double grade = rand.NextDouble();
+        randoGrades.Add(grade);
+    }
+
+    return randoGrades;
+}
---
+++ In_Class_Examples/MethodsWithParallelArraysButWithClasses/Student.cs
@@ -0,0 +1,50 @@
+﻿
+class Student
+{
+    public string Name { get; set; }
+
+    public int Id { get; set; }
+    public List<double> Grades { get; set; }
+
+
+    public Student()
+    {
+        Name = "";
+        Id = 0;
+        Grades = new List<double>();
+    }
+
+    public void StudentInformation()
+    {
+        double average = 0;
+        double sum = 0;
+        foreach (double grade in Grades)
+        {
+            sum += grade;
+        }
+        //sum = grades.Sum();
+        //average = sum / grades.Count();
+        //average = grades.Average();
+        average = sum / Grades.Count;
+
+        Console.WriteLine($"{Name} ({Id}) has {Grades.Count.ToString("N")} # of grades and has an average of {average.ToString("P")}!");
+    }
+
+    public override string ToString()
+    {
+            double average = 0;
+            double sum = 0;
+            foreach (double grade in Grades)
+            {
+                sum += grade;
+            }
+            //sum = grades.Sum();
+            //average = sum / grades.Count();
+            //average = grades.Average();
+            average = sum / Grades.Count;
+
+            return $"{Name} ({Id}) has {Grades.Count.ToString("N")} # of grades and has an average of {average.ToString("P")}!";
+        }
+    }
+
+
