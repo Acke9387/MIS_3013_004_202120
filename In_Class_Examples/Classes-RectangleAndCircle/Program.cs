a14c129b949afc241f5f1658c0b5b45a358afe3f
Acke9387 <adam@ou.edu>
2022-04-14 11:05:29 -0500

    Did an in class example over Rectangle

---
+++ In_Class_Examples/Classes-RectangleAndCircle/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Rectangle r1 = new Rectangle();
+r1.Length = 7;
+r1.Width = 10;
+//Console.WriteLine($"A rectangle with length: {r1.Length} & width {r1.Width} has an area of {r1.CalculateArea()} ");
+Console.WriteLine(r1);
+
+
+r1.Length = 10;
+r1.Width = 5;
+//Console.WriteLine($"A rectangle with length: {r1.Length} & width {r1.Width} has an area of {r1.CalculateArea()} ");
+Console.WriteLine(r1);
+Rectangle r2 = new Rectangle();
+r2.Length = 2;
+r2.Width = 3;
+//Console.WriteLine($"A rectangle with length: {r2.Length} & width {r2.Width} has an area of {r2.CalculateArea()} ");
+Console.WriteLine(r2);
+
+Rectangle r3 = new Rectangle(4, 8);
+
+Console.WriteLine(r3);
---
+++ In_Class_Examples/Classes-RectangleAndCircle/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿
+internal class Rectangle
+{
+
+    public double Length { get; set; }
+
+    public double Width { get; set; }
+
+    /// <summary>
+    /// Default/Empty Constructor
+    /// </summary>
+    public Rectangle()
+    {
+        Length = 7;
+        Width = 7;
+    }
+
+    /// <summary>
+    /// Overloaded Constructor
+    /// </summary>
+    /// <param name="length"></param>
+    /// <param name="width"></param>
+    public Rectangle(double length, double width)
+    {
+        Length= length;
+        Width= width;
+    }
+
+    public double CalculateArea()
+    {
+        return Width * Length;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * (Length + Width);
+    }
+
+    public double CalculateAreaForXRectangles(int x)
+    {
+        double areaForAllrects = (CalculateArea() * x);
+
+        return areaForAllrects;
+    }
+
+    public override string ToString()
+    {
+        return $"A rectangle with length: {Length} AND width {Width} has an area of {CalculateArea()} ";
+    }
+
+}
+
