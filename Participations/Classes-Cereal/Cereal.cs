0e8776e029fa851ffc90d791c02121acec244a28
Acke9387 <adam@ou.edu>
2022-04-26 10:50:19 -0500

    Completed the cereal participation

---
+++ Participations/Classes-Cereal/Cereal.cs
@@ -0,0 +1,26 @@
+﻿
+public class Cereal
+{
+    public string Manufacturer{get;set;}
+   
+    public string Name{get;set;}
+    
+    public double Calories{get;set;}
+
+    public double Cups { get; set; }
+
+
+    public Cereal()
+    {
+        Manufacturer = String.Empty;
+        Name = String.Empty;
+        Calories = 0;
+        Cups = 0;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} is made by {Manufacturer} and has {Calories.ToString("N")} per {Cups.ToString("N")} cups.";
+    }
+
+}
--- Participations/Classes-Cereal/Program.cs
+++ Participations/Classes-Cereal/Program.cs
@@ -1,6 +1,14 @@
 ﻿
 string[] linesOfFile = File.ReadAllLines("Cereal_Data.txt");
 
+List<Cereal> allCereals = new List<Cereal>();
+
+//Cereal cereal = new Cereal();
+//cereal.Name = "Raisin Bran";
+//cereal.Manufacturer = "Kelloggs";
+//cereal.Cups = 5;
+//cereal.Calories = 102;
+
 
 // Go through each line of the file (except for the first one)
 for (int i = 1; i < linesOfFile.Length; i++)
@@ -15,16 +23,39 @@ for (int i = 1; i < linesOfFile.Length; i++)
     //partsOfLine[3] : "0.33"
 
     // Processes pieces and create a Cereal
+    Cereal temp = new Cereal();
+    temp.Name = partsOfLine[0];
+    temp.Manufacturer = partsOfLine[1];
+    temp.Calories = Convert.ToDouble(partsOfLine[2]);
+    temp.Cups =     Convert.ToDouble(partsOfLine[3]);
+
 
 
     // Add cereal to a collection
-
+    allCereals.Add(temp);
 }
 
-
-
-
-
-
-
 // Output based upon the criteria in problem.
+
+//The application should output all of the Cereal information that have a serving size that is 1 cup or more.
+Console.WriteLine("All cereals with a serving size of 1 cup or more");
+
+foreach (Cereal c in allCereals)
+{
+    if (c.Cups >= 1)
+    {
+        Console.WriteLine($"\t" + c); 
+    }
+}
+
+//After you output all those cereals, we also need to inform them which Cereals have 100 calories or less per serving.
+
+Console.WriteLine("All cereals with a 100 calories or less per serving");
+
+foreach (Cereal c in allCereals)
+{
+    if (c.Calories <= 100)
+    {
+        Console.WriteLine($"\t" + c);
+    }
+}
